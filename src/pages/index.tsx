cd872d15d539c776687ea98c4c8361d27a36ca89
vulongdoan0612 <longvuxautrai12@gmail.com>
2024-06-02 22:20:10 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,27 @@
+import cookie from "cookie";
+
 const Home = () => {
   return <div></div>;
+};
+export const getServerSideProps = async (context: any) => {
+  const cookies = context.req.headers.cookie;
+  const parsedCookies = cookies ? cookie.parse(cookies) : {};
+  const token = parsedCookies["access_token_cms"];
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: `/login`,
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: `/post`,
+        permanent: false,
+      },
+    };
+  }
 };
 export default Home;
